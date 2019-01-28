d6d473c849a96e1e9a381d8f16ddbfd5a85ef92c
koidekenta <30596400+koidekenta@users.noreply.github.com>
2019-01-28 09:38:20 +0000

    Update main.php

--- main.php
+++ main.php
@@ -1,5 +1,7 @@
 <?php
 
+$arr = [0,3.0,1,"",2,null,true,"str","key" => "value","key2" => [0,1]];
+
 function array_any_element_delete($delete_element, $arr){
   $new_arr = [];
   if(array_search($delete_element, $arr, true) !== false){
@@ -12,5 +14,28 @@ function array_any_element_delete($delete_element, $arr){
   }
   return false;
 }
+
+print_r(array_any_element_delete("str", $arr));
+
+/*
+
+Array
+(
+    [0] => 0
+    [1] => 3
+    [2] => 1
+    [3] => 
+    [4] => 2
+    [5] => 
+    [6] => 1
+    [key] => value
+    [key2] => Array
+        (
+            [0] => 0
+            [1] => 1
+        )
+
+
+*/
 
 ?>
