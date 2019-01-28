673848abeb8571f993a2ff4ed459623856e3af4e
koidekenta <30596400+koidekenta@users.noreply.github.com>
2019-01-28 07:39:49 +0000

    Create main.php
    配列の中から、指定の値を削除して、新たな配列を返します。指定の値が複数ある場合は、先頭にある値だけが削除された配列を返します。

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+
+$arr = [0,1,2,3,"str","key" => "value","key2" => [0,1]];
+
+function array_any_element_delete($delete_element, $arr){
+  $new_arr = [];
+  if(array_search($delete_element, $arr, true) !== false){
+    $discover_key = array_search($delete_element, $arr, true);
+    unset($arr[$discover_key]);
+    foreach($arr as $key => $value){
+      $new_arr[$key] = $value;
+    }
+    return $new_arr;
+  }
+  return false;
+}
+
+?>
